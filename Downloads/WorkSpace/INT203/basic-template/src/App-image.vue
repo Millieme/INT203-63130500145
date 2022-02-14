5eae44c6e8033b517bc8c4435ffcc39902b2fbae
Wichanart <wcnsk15@gmail.com>
2022-02-14 21:12:13 +0700

    update

---
+++ Downloads/WorkSpace/INT203/basic-template/src/App-image.vue
@@ -0,0 +1,15 @@
+<script setup>
+const headingStyle = 'color: blue'
+const paraStyle = 'text-decoration:underline'
+const imagePath = './images/IMG_4409.jpg'
+</script>
+ 
+<template>
+<h1 v-bind:style="headingStyle">INT203</h1>
+<p :style="paraStyle">Lect. Umaporn</p>
+
+</template>
+ 
+<style>
+
+</style>
---
+++ Downloads/WorkSpace/INT203/basic-template/src/App-v-text.vue
@@ -0,0 +1,13 @@
+<script setup>
+const msg = 'Hello'
+const counter = 1
+</script>
+ 
+<template>
+<h1>{{ msg.toLowerCase() }}</h1>
+<p v-text="(counter += 10)"></p>
+</template>
+ 
+<style>
+
+</style>
